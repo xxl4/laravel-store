adc526f5dcd0ad9a17bb18da16e661d72051f91c
ss <nice.lizhi@gmail.com>
2023-02-17 07:55:06 +0800

    fix remove the config

--- app/Libs/doudian/src/open/core/DoudianOpConfig.php
+++ app/Libs/doudian/src/open/core/DoudianOpConfig.php
@@ -2,8 +2,8 @@
 
 class DoudianOpConfig
 {
-    public $appKey="7049939469750896164";
-    public $appSecret="b8774d29-b8da-4e7d-8369-cadd414235bd";
+    public $appKey="";
+    public $appSecret="";
     public $httpConnectTimeout = 1000;
     public $httpReadTimeout = 5000;
     public $openRequestUrl = "https://openapi-fxg.jinritemai.com";
