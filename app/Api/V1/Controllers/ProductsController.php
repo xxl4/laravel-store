e0fb1e241464e79877b3c3fc9ee759586a329917
ss <nice.lizhi@gmail.com>
2023-02-19 17:42:58 +0800

    商品API数据内容

---
+++ app/Api/V1/Controllers/ProductsController.php
@@ -0,0 +1,49 @@
+<?php
+namespace App\Api\V1\Controllers;
+
+use Dingo\Api\Routing\Helpers;
+use Illuminate\Routing\Controller;
+use Illuminate\Http\Request;
+use App\Libs\Utils;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Cache;
+
+/**
+ * stock resource representation
+ * @Resource("Stock", uri="/stock")
+ */
+class ProductsController extends Controller {
+    use Helpers;
+
+    /*
+     * 添加商品
+     * @Versions({"v1"})
+     * @Request()
+     * @Response()
+    */
+    public function add(Request $request) {
+
+    }
+
+    /*
+     * 编辑库存
+     * @Versions({"v1"})
+     * @Request()
+     * @Response()
+    */
+    public function edit(Request $request) {
+
+    }
+
+    /*
+     * 删除删除
+     * @Versions({"v1"})
+     * @Request()
+     * @Response()
+    */
+    public function delete(Request $request) {
+
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,32 @@ use Illuminate\Routing\Router;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group([
+    'namespace'     => "\App\Http\Controllers\Jos",
+    'middleware'    => ['web'],
+], function (Router $router) {
+    $router->any("v1/Jos/order/lists/{page}/{pagesize}", "OrderController@syncOrder");
+
+
+});
+
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', function ($api) {
+    $api->post('stock/add', 'App\Api\V1\Controllers\StockController@add');
+    $api->post('stock/down', 'App\Api\V1\Controllers\StockController@down');
+
+    # 商品操作
+    $api->post('product/add', 'App\Api\V1\Controllers\ProductsController@add');
+    $api->put('product/edit', 'App\Api\V1\Controllers\ProductsController@edit');
+    $api->delete('product/delete', 'App\Api\V1\Controllers\ProductsController@delete');
+
+    
+
+});
+
+$api->version('v2', function ($api) {
+    $api->post('stock/add', 'App\Api\V1\Controllers\StockController@down');
+    //$api->post('stock/down', 'App\Api\V1\Controllers\StockController@down');
+});
