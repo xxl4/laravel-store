d1a4574d974733bdf3e2832e4328d2ca8750cdc4
ss <nice.lizhi@gmail.com>
2023-03-01 16:52:05 +0800

    TB

---
+++ app/Console/Commands/Products/TB/Online/AddProduct.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands\Products\TB\Online;
+
+use Illuminate\Console\Command;
+
+class AddProduct extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'products.TB.add.online {store} {prod_id}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        ///
+        $store = $this->argument('store');
+        $prod_id = $this->argument("prod_id");
+        //
+        $this->info("get from online info start".$store);
+        $c = new \TopClient();
+        $c->appkey = $store->key;
+        $c->secretKey = $store->scret;
+
+        //获取对应商品的内容，完成商品的数据更新
+    }
+}
