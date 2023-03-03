3c7e74f6487ab10b2593a5190367c8bdd176c79b
ss <nice.lizhi@gmail.com>
2023-03-03 16:24:22 +0800

    #17 用戶模塊内容調整

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ class User extends Model
     //
     public $table = "user";
     public $primaryKey = "user_id";
+    public $keyType = "string";
     
     const UPDATED_AT = "modify_time";
     const CREATED_AT = "user_regtime";
