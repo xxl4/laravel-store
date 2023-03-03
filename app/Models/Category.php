6c042d11dff7427935fbbd65ed549b87fd029816
ss <nice.lizhi@gmail.com>
2023-03-03 22:00:55 +0800

    分类添加上自动ID

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,7 +10,7 @@ class Category extends Model
     public $table = "category";
     const UPDATED_AT = "update_time";
     const CREATED_AT = "rec_time";
-    public $primaryKey = "category_id";
+    //public $primaryKey = "category_id";
 
     public function prop() {
         return $this->belongsToMany(ProdProp::class,"category_prop","category_id","prop_id");
