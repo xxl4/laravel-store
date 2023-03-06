c5d314ad51090862216ea6a46f2c8a9d8288a80c
Steven Liu <steven@zylinkus.com>
2023-03-06 17:01:14 +0800

    #37 订单获取与售后获取列表

--- app/Console/Commands/Orders/TM/Online/GetOrders.php
+++ app/Console/Commands/Orders/TM/Online/GetOrders.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands\Orders\TM\Online;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 
 class GetOrders extends Command
 {
@@ -44,6 +45,15 @@ class GetOrders extends Command
         //
         $store = $this->argument('store');
         $order_id = $this->argument('order_id'); // 但order_id 为 0 的时候获取全部的
+        $outer_order_id = 0;
+        if($order_id > 0) {
+            $outer = \App\Models\Order::where("order_id", $order_id)->where("shop_id", $store->id)->select(["order_number"])->first();
+            if(is_null($outer)) {
+                //todo
+                return false;
+            }
+            return $this->OrderDetail($outer->order_number, $store);
+        }
         $this->info("get from online info start".$store);
         
         $this->getOnline(1, $store);
@@ -73,6 +83,10 @@ class GetOrders extends Command
 
         if (!property_exists($resp, 'trades')) {
             //todo 
+            var_dump($resp);
+            Log::error(json_encode($resp));
+            $this->error("数据好像给出差了哦");
+            return false;
         }
         $this->total = $resp->total_results;
         foreach($resp->trades->trade as $key=>$item) {
@@ -132,5 +146,83 @@ class GetOrders extends Command
                 
             }
         }
+    }
+
+    private function OrderDetail($outer_id, $store) {
+        $c = new \TopClient();
+        $c->appkey = $store->key;
+        $c->secretKey = $store->secret;
+
+        $req = new \TradeFullinfoGetRequest();
+        //$req->setStatus('WAIT_SELLER_SEND_GOODS'); //获取订单状态
+        $req->setFields($this->fields);
+        $req->setTid($outer_id);
+        $resp = $c->execute($req, $store->token);
+        //var_dump($resp);
+        if (!property_exists($resp, 'trade')) {
+            //todo 
+            var_dump($resp);
+            Log::error(json_encode($resp));
+            $this->error("数据好像给出差了哦");
+            return false;
+        }
+        $item = $resp->trade;
+
+        $order = \App\Models\Order::where("order_number", $item->tid)->first();
+        if(is_null($order)) $order = new \App\Models\Order();
+        $order->shop_id = $store->id;
+        $order->order_number = $item->tid;
+
+        $order->total = $item->total_fee;
+        $order->actual_total = $item->payment;
+        $order->freight_amount = $item->post_fee;
+
+        $remark = "";
+        $remark.="卖家的小旗帜为:".$item->seller_flag;
+        $remark.="买家是否有留意:".$item->has_buyer_message;
+
+        $order->remarks = $remark;
+
+        $order->product_nums = count($item->orders);
+        $order->pay_type = 1;
+
+        $order->status = \App\Enums\TM\OrderState::fromKey($item->status);
+
+
+
+        $user = \App\Models\User::where("outer_id",$item->buyer_open_uid)->first();
+        if(is_null($user)) $user = new \App\Models\User();
+        $user->outer_id = $item->buyer_open_uid;
+        $user->nick_name = $item->buyer_nick;
+        $user->status = 1;
+        $user->save();
+        $order->user_id = $user->user_id;
+
+        if(isset($item->pay_time)) $order->pay_time = $item->pay_time;
+        $order->reduce_amount = $item->discount_fee;
+
+        $order->save();
+
+        //订单商品的同步
+        foreach ($item->orders->order as $key=> $good) {
+            //var_dump($good);
+            $goods_id = str_replace('sd', '', $good->outer_sku_id);
+            $orderItem = \App\Models\OrderItem::where("shop_id", $store->id)->where("order_number", $item->tid)->where("prod_id", $goods_id)->first();
+            if(is_null($orderItem)) $orderItem = new \App\Models\OrderItem();
+            $orderItem->shop_id = $store->id;
+            $orderItem->order_number = $item->tid;
+            $orderItem->prod_count = $good->num;
+            $orderItem->sku_id = (int)$good->sku_id;
+            $orderItem->prod_id = (int)$goods_id;
+            $orderItem->sku_name = $good->sku_properties_name;
+            $orderItem->prod_name = $good->title;
+            $orderItem->price = $good->price;
+            $orderItem->user_id = $user->user_id;
+            $orderItem->product_total_amount = $good->total_fee;
+
+            $orderItem->save();
+            
+        }
+
     }
 }
---
+++ app/Console/Commands/Refund/TM/GetRefund.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\Console\Commands\Refund\DD;
+
+use Illuminate\Console\Command;
+use App\Libs\Utils;
+use Illuminate\Support\Facades\Log;
+
+class GetRefund extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'refund:TM:get:online {store} {refund_id}';
+
+    private $total = 0; // 订单总量
+    private $size = 100; // 订单数量
+
+    private $fields="refund_id, tid, title, buyer_nick, seller_nick, total_fee, status, created, refund_fee, oid, good_status, company_name, sid, payment, reason, desc, has_good_return, modified, order_status,refund_phase,refund_version,outer_id,attribute,sku,num";
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '获取天猫售后内容';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+        $store = $this->argument('store');
+        $refund_id = $this->argument('refund_id'); // 但prod_id 为 0 的时候获取全部的商品数据
+        $outer_refund_id = 0;
+        if($outer_refund_id > 0) {
+            $outer = \App\Models\RefundOrder::where("order_id", $order_id)->select(["order_number"])->first();
+            if(is_null($outer)) {
+                //todo
+                return false;
+            }
+        }
+
+        $this->getOnline(1,  $store);
+        //exit;
+
+        if($this->total > $this->size) {
+            $pages = ceil($this->total / $this->size);
+            //前面获取了第一页面数据了，所以从第二页开始
+            for($i=2; $i<=$pages; $i++) {
+                $this->getOnline($i,  $store);
+            }
+        }
+    }
+
+    public function getOnline($page, $store) {
+        $this->info($page);
+        //RefundsReceiveGetRequest
+        $c = new \TopClient();
+        $c->appkey = $store->key;
+        $c->secretKey = $store->secret;
+
+        $req = new \RefundsReceiveGetRequest();
+        //$req->setStatus('WAIT_SELLER_SEND_GOODS'); //获取订单状态
+        $req->setFields($this->fields);
+        $req->setPageSize($this->size);
+        $req->setPageNo($page);
+        $resp = $c->execute($req, $store->token);
+        //var_dump($resp);
+        if (!property_exists($resp, 'refunds')) {
+            //todo 
+            var_dump($resp);
+            Log::error(json_encode($resp));
+            $this->error("数据好像给出差了哦");
+            return false;
+        }
+        $this->total = $resp->total_results;
+
+        foreach($resp->refunds->refund as $key=>$item) {
+            //var_dump($localOrder);exit;
+
+            $refund = \App\Models\OrderRefund::where("refund_sn", $item->refund_id)->first();
+            if(is_null($refund)) $refund = new \App\Models\OrderRefund();
+            $refund->shop_id =  $store->id;
+            $refund->refund_sn =  $item->refund_id;
+            $refund->order_number =  $item->tid;
+
+            $user = \App\Models\User::where("outer_id",$item->buyer_open_uid)->first();
+            if(is_null($user)) $user = new \App\Models\User();
+            $user->outer_id = $item->buyer_open_uid;
+            $user->nick_name = $item->buyer_nick;
+            $user->status = 1;
+            $user->save();
+
+            $localOrder = \App\Models\Order::where("order_number", $item->tid)->first();
+            if(is_null($localOrder)) {
+                $localOrder = new \App\Models\Order();
+                $localOrder->order_number = $item->tid;
+                $localOrder->user_id = $user->user_id;
+                $localOrder->save();
+            }
+
+            $refund->user_id = $user->user_id;
+            $refund->order_amount = $item->total_fee;
+            $refund->refund_amount = $item->refund_fee;
+            $refund->buyer_msg = $item->reason;
+            $refund->goods_num = $item->num;
+
+            $refund->apply_time = $item->created;
+            $refund->order_id = $localOrder->order_id;
+
+            $refund->flow_trade_no = $item->refund_id;
+
+            $refund->outer_refund = json_encode($item);
+
+
+            $refund->save();
+        }  
+    }
+
+    private function getDetail($refund_id){
+        
+    }
+}
