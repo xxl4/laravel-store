1038fb5093124c3e29c2cd6ddd1983db7eecade8
ss <steven@zylinkus.com>
2023-03-23 21:28:20 +0800

    天猫商品的sku 添加与编辑操作，基于sku 表做好映射处理

--- app/Console/Commands/Products/TM/Online/AddSku.php
+++ app/Console/Commands/Products/TM/Online/AddSku.php
@@ -39,8 +39,34 @@ class AddSku extends Command
     {
         $store = $this->argument('store');
         $prod_id = $this->argument('prod_id'); // 但prod_id 为 0 的时候获取全部的商品数据
+
+        $prodouter = \App\Models\ProdOuter::where("prod_id", $this->prod_id)->select("outer_id")->first();
+        if(is_null($prodouter)) {
+            echo $this->prod_id." is not online now ";
+            return false;
+        }
+        if(empty($prodouter->outer_id)) {
+            echo $this->prod_id." is not online now ";
+            return false;
+        }
         
         
+
+        $c = new \TopClient();
+        $c->appkey = $store->key;
+        $c->secretKey = $store->secret;
+        //@link https://open.taobao.com/api.htm?docId=27&docType=2&source=search
+        $req = new \ItemSkuAddRequest();
+        $req->setNumIid($prodouter->outer_id);
+        $req->setOuterId($prodouter->outer_id);
+        $req->setPrice($prodouter->outer_id);
+        $req->setProperties($prodouter->outer_id);
+        $req->setQuantity($prodouter->outer_id);
+        $req->setItemPrice($prodouter->outer_id);
+        //$req->setNumIids(0);
+        //$req->setFields($this->_fields);
+        
+        $resp = \App\Libs\Utils::execThirdStoreApi($store->id, $c, $store->token, $req);
           
     }
 
--- app/Console/Commands/Products/TM/Online/EditSku.php
+++ app/Console/Commands/Products/TM/Online/EditSku.php
@@ -40,42 +40,32 @@ class EditSku extends Command
         $store = $this->argument('store');
         $prod_id = $this->argument('prod_id'); // 但prod_id 为 0 的时候获取全部的商品数据
         
-        
-          
-    }
-
-    
-
-    public function GetDetail($outer_id, $access_token, $store) {
+        //@link https://open.taobao.com/api.htm?docId=29&docType=2&source=search
         echo $outer_id."\r\n";
         $c = new \TopClient();
         $c->appkey = $store->key;
         $c->secretKey = $store->secret;
 
-        $req = new \ItemSkusGetRequest();
-        $req->setNumIids($outer_id);
-        //$req->setNumIids(0);
-        $req->setFields($this->_fields);
+        $prodouter = \App\Models\ProdOuter::where("prod_id", $this->prod_id)->select("outer_id")->first();
+        if(is_null($prodouter)) {
+            echo $this->prod_id." is not online now ";
+            return false;
+        }
+        if(empty($prodouter->outer_id)) {
+            echo $this->prod_id." is not online now ";
+            return false;
+        }
+
+        $req = new \ItemSkuUpdateRequest();
+        $req->setNumIid($prodouter->outer_id);
+        $req->setOuterId($prodouter->outer_id);
+        $req->setPrice($prodouter->outer_id);
+        $req->setProperties($prodouter->outer_id);
+        $req->setQuantity($prodouter->outer_id);
+        $req->setItemPrice($prodouter->outer_id);
         
         $resp = \App\Libs\Utils::execThirdStoreApi($store->id, $c, $store->token, $req);
-        
-        foreach($resp->skus->sku as $key=>$sku) {
-            $sku_mapping = \App\Models\ProdSkuOuter::where("outer_prod_id", $sku->iid)->where("outer_sku_id", $sku->sku_id)->first();
-            if(is_null($sku_mapping)) $sku_mapping = new \App\Models\ProdSkuOuter();
-
-            $status = 1;
-
-            $sku_mapping->outer_prod_id = $sku->iid;
-            $sku_mapping->outer_sku_id = $sku->sku_id;
-            $sku_mapping->sku_id = $sku->outer_id;
-            $sku_mapping->quantity = $sku->quantity;
-            $sku_mapping->properties_name = $sku->properties_name;
-            if($sku->status=='delete') $status = 0;
-            $sku_mapping->status = $status;
-
-            $sku_mapping->save();
-        }
-        
-        
     }
+
+
 }
