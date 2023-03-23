303261ec949f556ddb8e5f1ae8fc8c84a131a438
ss <steven@zylinkus.com>
2023-03-23 21:16:58 +0800

    天猫商品SKU操作

---
+++ app/Console/Commands/Products/TM/Online/DeleteSku.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Console\Commands\Products\TM\Online;
+
+use Illuminate\Console\Command;
+use App\Libs\Utils;
+
+class DeleteSku extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'products:TM:delete_sku:online {store} {prod_id} {data?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '删除商品的SKU内容';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $store = $this->argument('store');
+        $prod_id = $this->argument('prod_id'); // 但prod_id 为 0 的时候获取全部的商品数据
+        
+        
+          
+    }
+
+    
+
+    public function GetDetail($outer_id, $access_token, $store) {
+        echo $outer_id."\r\n";
+        $c = new \TopClient();
+        $c->appkey = $store->key;
+        $c->secretKey = $store->secret;
+
+        $req = new \ItemSkusGetRequest();
+        $req->setNumIids($outer_id);
+        //$req->setNumIids(0);
+        $req->setFields($this->_fields);
+        
+        $resp = \App\Libs\Utils::execThirdStoreApi($store->id, $c, $store->token, $req);
+        
+        foreach($resp->skus->sku as $key=>$sku) {
+            $sku_mapping = \App\Models\ProdSkuOuter::where("outer_prod_id", $sku->iid)->where("outer_sku_id", $sku->sku_id)->first();
+            if(is_null($sku_mapping)) $sku_mapping = new \App\Models\ProdSkuOuter();
+
+            $status = 1;
+
+            $sku_mapping->outer_prod_id = $sku->iid;
+            $sku_mapping->outer_sku_id = $sku->sku_id;
+            $sku_mapping->sku_id = $sku->outer_id;
+            $sku_mapping->quantity = $sku->quantity;
+            $sku_mapping->properties_name = $sku->properties_name;
+            if($sku->status=='delete') $status = 0;
+            $sku_mapping->status = $status;
+
+            $sku_mapping->save();
+        }
+        
+        
+    }
+}
