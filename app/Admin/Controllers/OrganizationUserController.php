9aaf36928081f38907a26d4de1c35c66128f9e44
ss <nice.lizhi@gmail.com>
2023-03-25 16:05:23 +0800

    #17 对于生成机构可以加上消息的发送处理，从而用户可以做到具体的内容

--- app/Admin/Controllers/OrganizationUserController.php
+++ app/Admin/Controllers/OrganizationUserController.php
@@ -96,23 +96,27 @@ class OrganizationUserController extends AdminController
 
         $form->saving(function (Form $form) {
             $userModel = config('admin.database.users_model');
-            $password = "123456";
+            $password = Str::random(6);
+            $org_code = \App\Models\Organization::where("id", Admin::user()->org_id)->value("code");
+            $username = $org_code."@".Str::random(6);
 
             //$new_password = Hash::make($password);
 
             $user = new $userModel();
-            $user->username = "usa@".Str::random(6);
+            $user->username = $username;
             $user->password = Hash::make($password);
             //$user->status = 1;
             $user->name = $form->name;
             $user->save();
             $form->user_id = $user->id;
+
+            \App\Libs\Utils::sendMessage(1, Admin::user()->id,"恭喜您用户创建成功","恭喜您创建用户成功，您的登录名:".$username." 与登录密码为:".$password." 您可以登录后重新设置您的登录信息");
+
         });
 
         // 数据保存后的权限调整
         $form->saved(function (Form $form) {
             // 创建用户
-            
             //$user->remember_token = $form->model()->username;
             // 清理前面的权限
             DB::table("admin_role_users")->where('user_id', $form->model()->user_id)->delete();
