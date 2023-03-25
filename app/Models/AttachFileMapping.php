cba3b5c587c4a6817680abf1048814014acc12c3
ss <nice.lizhi@gmail.com>
2023-03-25 10:58:53 +0800

    系统附件上传同步内容

---
+++ app/Models/AttachFileMapping.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AttachFileMapping extends Model
+{
+    //
+    public $table = "attach_mapp";
+    //public $fillable = ["file_path"];
+}
