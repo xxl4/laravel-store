3adecce74a8002a784d8a1345ec95c125dac767c
ss <nice.lizhi@gmail.com>
2023-03-26 19:22:45 +0800

    #3 删除不健康的前端代码

--- app/Admin/bootstrap.php
+++ app/Admin/bootstrap.php
@@ -4,7 +4,7 @@ use Nicelizhi\Admin\Form;
 
 Nicelizhi\Admin\Form::forget(['map', 'editor']);
 //app('view')->prependNamespace('admin', resource_path('views/admin'));
-Form::extend('editor', WangEditor::class);
+//Form::extend('editor', WangEditor::class);
 
 //Admin::css('https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css');
 //Admin::js('https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js');
