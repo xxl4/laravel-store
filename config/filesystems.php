a9a8e1fbaf0fb0dd186c26f2ed5b227975b6880f
ss <nice.lizhi@gmail.com>
2023-03-28 16:05:49 +0800

    oss env config

--- config/filesystems.php
+++ config/filesystems.php
@@ -68,12 +68,12 @@ return [
         ],
         'oss' => [
             'driver'        => 'oss',
-            'access_id'     => 'LTAII8gcaVHlHv8L',
-            'access_key'    => 'Fp7rXdm8HXi6nsFeHOOE44LoWuGTPn',
-            'bucket'        => 'sdimage',
+            'access_id'     => env('OSS_ACCESS_ID'),
+            'access_key'    => env('OSS_ACCESS_KEY'),
+            'bucket'        => env('OSS_BUCKET'),
             'endpoint'      => 'oss-cn-shanghai.aliyuncs.com', // OSS 外网节点或自定义外部域名
             'endpoint_internal' => 'oss-cn-shanghai-internal.aliyuncs.com', // v2.0.4 新增配置属性，如果为空，则默认使用 endpoint 配置(由于内网上传有点小问题未解决，请大家暂时不要使用内网节点上传，正在与阿里技术沟通中)
-            'cdnDomain'     => 'https://sdimage.oss-cn-shanghai.aliyuncs.com/', // 如果isCName为true, getUrl会判断cdnDomain是否设定来决定返回的url，如果cdnDomain未设置，则使用endpoint来生成url，否则使用cdn
+            'cdnDomain'     => env('OSS_CDN_DOMAIN'), // 如果isCName为true, getUrl会判断cdnDomain是否设定来决定返回的url，如果cdnDomain未设置，则使用endpoint来生成url，否则使用cdn
             'ssl'           => false, // true to use 'https://' and false to use 'http://'. default is false,
             'isCName'       => false, // 是否使用自定义域名,true: 则Storage.url()会使用自定义的cdn或域名生成文件url， false: 则使用外部节点生成url
             'debug'         => false
